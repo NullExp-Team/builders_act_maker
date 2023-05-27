0f96cc3dee14db93417aa836f47696eba6e8906d
iamgirya <iamgirya@yandex.ru>
2023-05-27 12:24:45 +0300

    create c# project

---
+++ c#/ActBuilder/FlutterAdapter.cs
@@ -0,0 +1,30 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ActBuilder
+{
+    /// <summary>
+    /// Хранит все методы, которые могут вызываться через FFI
+    /// Одновременно роль фасада - то есть определяет методы, которые объединяют нужные методы рабочих классов
+    /// </summary> 
+    class FlutterAdapter
+    {
+        [UnmanagedCallersOnly(EntryPoint = "add")]
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        [UnmanagedCallersOnly(EntryPoint = "ops")]
+        public static int Ops(int a, int b)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var packages = new ExcelPackage();
+            var sheet = packages.Workbook.Worksheets.Add("Market Report");
+            var byt = packages.GetAsByteArray();
+
+            File.WriteAllBytes("Report.xlsx", byt);
+            return 8;
+        }
+    }
+}
---
+++ c#/ActBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ActBuilder
+{
+    /// <summary>
+    ///  Имеет метод мейн, через который осуществляется отладка написанных функций.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
