a78b819df51cc51eaf9edd5218850d66b6589852
StarProxima <maks.prozorov1@yandex.ru>
2023-05-31 02:04:10 +0300

    Smooth grid scroll

--- flutter/lib/features/closure/presentation/closure_detail_screen/ui/closure_detail_screen.dart
+++ flutter/lib/features/closure/presentation/closure_detail_screen/ui/closure_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dyn_mouse_scroll/dyn_mouse_scroll.dart';
+
 import '../../../../../core/theme/theme_context_extension.dart';
 import 'widgets/act_card.dart';
 import 'package:flutter_reorderable_grid_view/widgets/reorderable_builder.dart';
@@ -26,15 +28,7 @@ class ClosureDetailScreen extends StatefulWidget {
 }
 
 class _ClosureDetailScreenState extends State<ClosureDetailScreen> {
-  final scrollController = ScrollController();
-
   final gridKey = GlobalKey();
-
-  @override
-  void dispose() {
-    scrollController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,64 +49,89 @@ class _ClosureDetailScreenState extends State<ClosureDetailScreen> {
             return ScaffoldPage(
               header: NavigationHeader(routes: routes),
               content: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                padding: const EdgeInsets.symmetric(vertical: 16),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      closure.name,
-                      style: context.textStyles.title,
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      closure.path,
-                      style: context.textStyles.bodyLarge?.copyWith(
-                        color: Colors.grey[80],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  closure.name,
+                                  style: context.textStyles.titleLarge,
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  closure.path,
+                                  style: context.textStyles.body?.copyWith(
+                                    color: Colors.grey[80],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 32),
+                          const Flexible(
+                            child: InfoBar(
+                              title: Text('Справка'),
+                              content: Padding(
+                                padding: EdgeInsets.only(right: 6),
+                                child: Text(
+                                  'Можно менять порядок элементов, зажимая и перетаскивая их.',
+                                ),
+                              ),
+                              severity: InfoBarSeverity.info,
+                              isLong: true,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    const InfoBar(
-                      title: Text('Справка'),
-                      content: Padding(
-                        padding: EdgeInsets.only(right: 6),
-                        child: Text(
-                          'Можно менять порядок актов, зажимая и перетаскивая их.',
-                        ),
-                      ),
-                      severity: InfoBarSeverity.info,
-                      isLong: true,
                     ),
                     const SizedBox(height: 16),
                     Expanded(
-                      child: ReorderableBuilder(
-                        scrollController: scrollController,
-                        onReorder: (orderUpdateEntities) => cubit.reorderGrid(
-                          orderUpdateEntities.map(
-                            (e) => (oldIndex: e.oldIndex, newIndex: e.newIndex),
+                      child: DynMouseScroll(
+                        builder: (context, controller, physics) =>
+                            ReorderableBuilder(
+                          scrollController: controller,
+                          onReorder: (orderUpdateEntities) => cubit.reorderGrid(
+                            orderUpdateEntities.map(
+                              (e) =>
+                                  (oldIndex: e.oldIndex, newIndex: e.newIndex),
+                            ),
                           ),
-                        ),
-                        builder: (children) => GridView.builder(
-                          key: gridKey,
-                          controller: scrollController,
-                          gridDelegate:
-                              const SliverGridDelegateWithMaxCrossAxisExtent(
-                            maxCrossAxisExtent: 400,
-                            crossAxisSpacing: 16,
-                            mainAxisSpacing: 16,
-                            childAspectRatio: 2.25,
+                          builder: (children) => GridView.builder(
+                            key: gridKey,
+                            physics: physics,
+                            controller: controller,
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 16,
+                              horizontal: 32,
+                            ),
+                            gridDelegate:
+                                const SliverGridDelegateWithMaxCrossAxisExtent(
+                              maxCrossAxisExtent: 400,
+                              crossAxisSpacing: 16,
+                              mainAxisSpacing: 16,
+                              childAspectRatio: 2.25,
+                            ),
+                            itemCount: children.length,
+                            itemBuilder: (context, index) => children[index],
                           ),
-                          itemCount: children.length,
-                          itemBuilder: (context, index) => children[index],
+                          lockedIndices: [closure.acts.length],
+                          children: [
+                            for (var act in closure.acts)
+                              ActCard(key: ValueKey(act.hashCode), act: act),
+                            const CreateActCart(
+                              key: Key('create_act_cart'),
+                            )
+                          ],
                         ),
-                        lockedIndices: [closure.acts.length],
-                        children: [
-                          for (var act in closure.acts)
-                            ActCard(key: ValueKey(act.hashCode), act: act),
-                          const CreateActCart(
-                            key: Key('create_act_cart'),
-                          )
-                        ],
                       ),
                     )
                   ],
