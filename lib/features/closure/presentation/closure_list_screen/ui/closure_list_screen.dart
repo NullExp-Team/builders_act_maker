c4faa6154d0be19e61d5d7c9b9cc606b2063efd7
iamgirya <iamgirya@yandex.ru>
2023-06-05 01:18:40 +0300

    fast add loading screen

--- lib/features/closure/presentation/closure_list_screen/ui/closure_list_screen.dart
+++ lib/features/closure/presentation/closure_list_screen/ui/closure_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import '../../../../../core/widgets/navigation_header.dart';
 import 'widgets/add_closure_card.dart';
 import 'package:fluent_ui/fluent_ui.dart';
@@ -16,51 +18,67 @@ class ClosureListScreen extends StatefulWidget {
 
 class _ClosureListScreenState extends State<ClosureListScreen> {
   late final cubit = context.read<ClosureListCubit>();
+  //Код пишется в час ночи, потому что прогер резко вспомнил про забытую задачу. За качество кода не отвечаю
+  bool isLoaded = false;
 
   @override
   void initState() {
+    Future.delayed(const Duration(seconds: 4)).then((value) {
+      isLoaded = true;
+      setState(() {});
+    });
     super.initState();
     cubit.loadClosures();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ScaffoldPage(
-      header: NavigationHeader(),
-      content: BlocBuilder<ClosureListCubit, ClosureListState>(
-        builder: (context, state) {
-          switch (state) {
-            case ClosureListStateInitial() || ClosureListStateLoading():
-              return const Center(child: ProgressRing());
-            case ClosureListStateData(:final closures):
-              return DynMouseScroll(
-                durationMS: 100,
-                builder: (context, controller, physics) => ListView.separated(
-                  controller: controller,
-                  physics: physics,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-                  itemCount: closures.length + 1,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return const Padding(
-                        padding: EdgeInsets.only(bottom: 16),
-                        child: AddClosureCard(),
+    if (isLoaded) {
+      return ScaffoldPage(
+        header: NavigationHeader(),
+        content: BlocBuilder<ClosureListCubit, ClosureListState>(
+          builder: (context, state) {
+            switch (state) {
+              case ClosureListStateInitial() || ClosureListStateLoading():
+                return const Center(child: ProgressRing());
+              case ClosureListStateData(:final closures):
+                return DynMouseScroll(
+                  durationMS: 100,
+                  builder: (context, controller, physics) => ListView.separated(
+                    controller: controller,
+                    physics: physics,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 16),
+                    itemCount: closures.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return const Padding(
+                          padding: EdgeInsets.only(bottom: 16),
+                          child: AddClosureCard(),
+                        );
+                      }
+                      return ClosureCard(
+                        closure: closures[index - 1],
                       );
-                    }
-                    return ClosureCard(
-                      closure: closures[index - 1],
-                    );
-                  },
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 6),
-                ),
-              );
-            case ClosureListStateError():
-              return const Center(child: Text('Ошибка'));
-          }
-        },
-      ),
-    );
+                    },
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(height: 6),
+                  ),
+                );
+              case ClosureListStateError():
+                return const Center(child: Text('Ошибка'));
+            }
+          },
+        ),
+      );
+    } else {
+      return ScaffoldPage(
+        content: Center(
+            child: Image.file(
+          File('assets/ring.gif'),
+          width: 350,
+        )),
+      );
+    }
   }
 }
